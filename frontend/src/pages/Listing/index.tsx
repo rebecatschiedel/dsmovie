2a5f70cb2c6c20cd4fd8651f549d17ce3cb851eb
rebecatschiedel <rebecatcmg@gmail.com>
2022-05-11 22:00:18 -0700

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,13 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <MovieCard />
+    </>
+  );
 }
 
 export default Listing;
